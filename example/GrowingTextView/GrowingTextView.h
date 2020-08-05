abfa2e76b24ceb4f1988051c1fd6dd450e120d85
Jay Moore <jmoore@ehealthinnovation.org>
2020-08-05 14:30:27 +0200

    add framework target, update to xcode 11

---
+++ example/GrowingTextView/GrowingTextView.h
@@ -0,0 +1,18 @@
+//
+//  GrowingTextView.h
+//  GrowingTextView
+//
+//  Created by Jay Moore on 2020-08-05.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GrowingTextView.
+FOUNDATION_EXPORT double GrowingTextViewVersionNumber;
+
+//! Project version string for GrowingTextView.
+FOUNDATION_EXPORT const unsigned char GrowingTextViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GrowingTextView/PublicHeader.h>
+
+
